7f89a802429392c940579cd84be8efd1c0a192ed
Vanessa Lane <vlane@23andme.com>
2020-08-29 23:33:43 -1000

    add comment-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-// const commentRoutes = require('./comment-routes.js');
+const commentRoutes = require('./comment-routes.js');
 const postRoutes = require('./post-routes.js');
 const userRoutes = require('./user-routes.js');
 
-// router.use('/comments', commentRoutes);
+router.use('/comments', commentRoutes);
 router.use('/posts', postRoutes);
 router.use('/users', userRoutes);
 
