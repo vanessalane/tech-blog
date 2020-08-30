f7e40b7fb0f90700615f1f759a567d9852314351
Vanessa Lane <vlane@23andme.com>
2020-08-29 22:28:23 -1000

    finish basic template

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+// const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
 
-router.get('/', (req, res) => {
-    res.render('homepage');
+router.use('/', homeRoutes);
+// router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
 });
-
+  
 module.exports = router;
