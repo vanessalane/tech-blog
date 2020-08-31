0d0289ccf364ba27456f96d05d4f317de1a00e82
Vanessa Lane <vlane@23andme.com>
2020-08-30 19:53:51 -1000

    minor changes while troubleshooting heroku

--- server.js
+++ server.js
@@ -5,18 +5,17 @@ const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
 const app = express();
-const PORT = process.env.PORT || 3301;
+const PORT = process.env.PORT || 3001;
 
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({helpers});
 
 const session = require('express-session');
-
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: "keyboard cat",
-  cookie: { maxAge: 600000 },  // session expires after 10 mins
+  secret: process.env.SECRET,
+  cookie: { maxAge: 8 * 60 * 60 * 1000 },  // session expires after 8 hours
   resave: false,
   rolling: true,
   saveUninitialized: true,
@@ -25,17 +24,17 @@ const sess = {
   })
 };
 
-app.use(session(sess));
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(session(sess));
 app.use(express.static((path.join(__dirname, 'public'))));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on port ${PORT}.`));
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}`));
 });
